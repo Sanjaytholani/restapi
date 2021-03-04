df9e3a917ff932135c38037809e5096c0978108c
Sanjaytholani <sanjaytholani.m@gmail.com>
2021-03-04 10:54:41 +0530

    restapi

---
+++ app.js
@@ -0,0 +1,31 @@
+require("dotenv/config");
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+const postRoutes = require("./routes/posts");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const url = process.env.DB_CONNECTION;
+const connectionParams = {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+};
+mongoose
+  .connect(url, connectionParams)
+  .then(() => {
+    console.log("Connected to database ");
+  })
+  .catch((err) => {
+    console.error(`Error connecting to the database. \n${err}`);
+  });
+
+app.use(bodyParser.json());
+app.use(cors());
+app.get("/", (req, res) => {
+  res.send("18BLC1090");
+});
+
+app.use("/posts", postRoutes);
+app.listen(3000);
